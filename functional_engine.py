6fe47505f863476e072b74437d9e7b61bea8a568
HamzaDev <hmzt25235@gmail.com>
2025-12-04 22:52:31 +0200

    Add template

---
+++ functional_engine.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+Grid = Tuple[Tuple[int, ...], ...]  # 0 = dead, 1 = alive
+
+# def create_grid(rows: int, cols: int) -> Grid: # 
+#     """Create a new immutable grid with all cells dead."""
+#     ...
+
+def get_cell(grid: Grid, row: int, col: int) -> int: # Nadeen
+    """Returns the current state of the cell"""
+    ...
+
+def count_neighbors(grid: Grid, row: int, col: int) -> int: # Hend
+    """Return the number of live neighbors around a given cell."""
+    ...
+
+def next_state(current_state: int, neighbors: int) -> int: # Abdo
+    """Return the next state (0 or 1) for a single cell."""
+    ...
+
+def next_generation(grid: Grid) -> Grid: # Mostafa, Yasmin
+    """Return a new immutable grid representing the next generation."""
+    ...
---
+++ imperative_engine.py
@@ -0,0 +1,7 @@
+from typing import List
+
+Grid = List[List[int, ...], ...]  # 0 = dead, 1 = alive
+
+def next_generation(grid: Grid) -> Grid: # Heidy
+    """Reassign Grid with a new mutable grid representing the next generation."""
+    ...
