9e8c89e6ffabd298b559f2bd26f31f73ee397f39
Hamza Elkotb <61008779+HamzaElkotp@users.noreply.github.com>
2025-12-06 06:07:19 +0200

    Update imperative_engine.py

--- imperative_engine.py
+++ imperative_engine.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import Tuple
 
-Grid = List[List[int, ...], ...]  # 0 = dead, 1 = alive
+Grid = Tuple[Tuple[int, ...], ...]  # 0 = dead, 1 = alive
 
 def next_generation(grid: Grid) -> Grid: # Heidy
     """Reassign Grid with a new mutable grid representing the next generation."""
