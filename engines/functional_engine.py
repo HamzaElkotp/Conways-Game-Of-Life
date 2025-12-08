1e02a3f5f5cab7ea8ffbd3c9c9305e41ee0cb5d1
HamzaDev <hmzt25235@gmail.com>
2025-12-09 01:37:24 +0200

    Update functional_engine.py

--- engines/functional_engine.py
+++ engines/functional_engine.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from functools import reduce
 
 Grid = Tuple[Tuple[int, ...], ...]  # 0 = dead, 1 = alive
 
@@ -17,16 +18,21 @@ neighbors:Tuple[Tuple[int, int]] = [
     ( 1, -1), ( 1, 0), ( 1, 1)
 ]
 
+"""
 #### We have 5 implementations for counting neigbors
+"""
 
+"""
 ########## METHOD1: Not very functional (uses loops)
+"""
 def count_neighbors(grid: Grid, row: int, col: int) -> int:
     neighbors_values = [get_cell(grid, row + dx, col + dy) for (dx, dy) in neighbors]
     result = sum(neighbors_values)
     return result
 
-
+"""
 ########## METHOD2: More functional (uses Recursive)
+"""
 def count_neighbors_recursive(grid: Grid, row: int, col: int, indx:int = 0) -> int:
     if indx >= 8:
         return 0
@@ -39,7 +45,9 @@ def count_neighbors_recursive(grid: Grid, row: int, col: int, indx:int = 0) -> i
         count_neighbors_recursive(grid, row, col, indx+1)
     )
 
+"""
 ########## METHOD3: More More functional (uses Tail Recursive)
+"""
 def count_neighbors_tail_recursive(grid: Grid, row: int, col: int, indx:int = 0, acc:int = 0) -> int:
     if indx >= 8:
         return acc
@@ -48,6 +56,28 @@ def count_neighbors_tail_recursive(grid: Grid, row: int, col: int, indx:int = 0,
 
     return count_neighbors_tail_recursive(grid, row, col, indx+1, new_acc)
 
+"""
+########## METHOD4: More More More functional (uses Higher order function + Tail Recursive)
+########## We implemented the array.reduce higher from scratch
+
+“ Always Try To ReInvent The Wheel ”
+- Hamza M. Elkotb, A CS Student @ FCAIH.
+"""
+def array_reduce(array, operation_func, accumulator:int, indx:int, *args, **kwargs):
+    if(indx>=len(array)):
+        return accumulator
+    new_acc = operation_func(accumulator, array[indx], *args, **kwargs) # pass accumulator and current
+    return array_reduce(array, operation_func, new_acc, indx+1, *args, **kwargs)
+
+def count_neighbors_with_accumelator(accumulator:int, array, row:int, col:int, grid:Grid):
+    return accumulator + get_cell(grid, row + array[0], col + array[1])
+
+"""
+# Example of usage
+meow:Grid = [[1,2,3], [4,5,6], [7,8,9]]
+res = array_reduce(neighbors, count_neighbors_with_accumelator, 0, 0, 1, 1, meow)
+print(res)
+"""
 
 def next_state(current_state: int, neighbors: int):
     if current_state == 1 and neighbors < 2: # (underpopulation)
