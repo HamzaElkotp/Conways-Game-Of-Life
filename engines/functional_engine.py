787d42eec380984245e66a5c2b057ca9fde9189f
HamzaDev <hmzt25235@gmail.com>
2025-12-09 01:18:56 +0200

    Update functional_engine.py

--- engines/functional_engine.py
+++ engines/functional_engine.py
@@ -11,22 +11,35 @@ def get_cell(grid: Grid, row: int, col: int) -> int:
    else: return grid[row][col]
 
 
-def count_neighbors(grid: Grid, row: int, col: int) -> int:
-    neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-    neighbors_values = [get_cell(grid, row + dx, col + dy) for (dx, dy) in neighbors]
-
-    result = sum(neighbors_values)
-
-    return result
-
-
 neighbors:Tuple[Tuple[int, int]] = [
     (-1, -1), (-1, 0), (-1, 1), 
     ( 0, -1),          ( 0, 1), 
     ( 1, -1), ( 1, 0), ( 1, 1)
 ]
 
+#### We have 5 implementations for counting neigbors
+
+########## METHOD1: Not very functional (uses loops)
+def count_neighbors(grid: Grid, row: int, col: int) -> int:
+    neighbors_values = [get_cell(grid, row + dx, col + dy) for (dx, dy) in neighbors]
+    result = sum(neighbors_values)
+    return result
+
+
+########## METHOD2: More functional (uses Recursive)
+def count_neighbors_recursive(grid: Grid, row: int, col: int, indx:int = 0) -> int:
+    if indx >= 8:
+        return 0
+    
+    neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    return (
+        get_cell(grid, row + neighbors[indx][0], col + neighbors[indx][1])
+        +
+        count_neighbors_recursive(grid, row, col, indx+1)
+    )
+
+########## METHOD3: More More functional (uses Tail Recursive)
 def count_neighbors_tail_recursive(grid: Grid, row: int, col: int, indx:int = 0, acc:int = 0) -> int:
     if indx >= 8:
         return acc
