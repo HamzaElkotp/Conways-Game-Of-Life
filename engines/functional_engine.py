4931a573a3f535d3868906010c631d0f32704607
HamzaDev <hmzt25235@gmail.com>
2025-12-09 00:34:22 +0200

    use tail recursion

--- engines/functional_engine.py
+++ engines/functional_engine.py
@@ -3,7 +3,7 @@ from typing import Tuple
 Grid = Tuple[Tuple[int, ...], ...]  # 0 = dead, 1 = alive
 
 
-def get_cell(grid: Grid, row: int, col: int) -> int: # Nadeen
+def get_cell(grid: Grid, row: int, col: int) -> int:
    if row<0 or row>= len(grid):
        return 0
    elif col<0 or col>=len(grid[0]):
@@ -11,7 +11,7 @@ def get_cell(grid: Grid, row: int, col: int) -> int: # Nadeen
    else: return grid[row][col]
 
 
-def count_neighbors(grid: Grid, row: int, col: int) -> int: # Hend
+def count_neighbors(grid: Grid, row: int, col: int) -> int:
     neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
     neighbors_values = [get_cell(grid, row + dx, col + dy) for (dx, dy) in neighbors]
@@ -21,17 +21,19 @@ def count_neighbors(grid: Grid, row: int, col: int) -> int: # Hend
     return result
 
 
-def count_neighbors_recursive(grid: Grid, row: int, col: int, indx:int = 0) -> int:
-    if indx >= 8:
-        return 0
-    
-    neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+neighbors:Tuple[Tuple[int, int]] = [
+    (-1, -1), (-1, 0), (-1, 1), 
+    ( 0, -1),          ( 0, 1), 
+    ( 1, -1), ( 1, 0), ( 1, 1)
+]
 
-    return (
-        get_cell(grid, row + neighbors[indx][0], col + neighbors[indx][1])
-        +
-        count_neighbors_recursive(grid, row, col, indx+1)
-    )
+def count_neighbors_tail_recursive(grid: Grid, row: int, col: int, indx:int = 0, acc:int = 0) -> int:
+    if indx >= 8:
+        return acc
+    
+    new_acc = acc + get_cell(grid, row + neighbors[indx][0], col + neighbors[indx][1])
+
+    return count_neighbors_tail_recursive(grid, row, col, indx+1, new_acc)
 
 
 def next_state(current_state: int, neighbors: int):
@@ -46,7 +48,7 @@ def next_state(current_state: int, neighbors: int):
     return 0
 
 
-def next_generation(grid: Grid) -> Grid: # Mostafa, Yasmin
+def next_generation(grid: Grid) -> Grid:
     rows = len(grid)
     cols = len(grid[0])
 
@@ -54,7 +56,8 @@ def next_generation(grid: Grid) -> Grid: # Mostafa, Yasmin
         tuple(
             next_state(
                 get_cell(grid, x, y),
-                count_neighbors(grid, x, y)
+                count_neighbors_tail_recursive(grid, x, y) 
+                # count_neighbors will work too, but count_neighbors_tail_recursive is morefunctional programming
             )
             for y in range(cols)
         )
